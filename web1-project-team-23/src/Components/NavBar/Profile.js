8054578dcd5f8135bc4f0da062ec841dac8053e8
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2023-12-17 02:48:36 +0200

    Navbar  & footer + website icon changed + some modifications for index.html & index.css have been made

---
+++ web1-project-team-23/src/Components/NavBar/Profile.js
@@ -0,0 +1,19 @@
+import "./NavBarStyle.css";
+const Profile = () =>{
+    return(
+        <>
+            <div className="profilePic">
+                <img
+                src="https://images.unsplash.com/photo-1522733603432-44910cfd6773?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+                className="object-fit-cover"></img>
+            </div>
+            <div className="d-none d-xl-block">
+                <span className="h6 stretched-link">Saif Khalifa</span>
+                <p className="small m-0">Web Developer</p>
+            </div>
+        </>
+    );
+}
+
+export default Profile;
