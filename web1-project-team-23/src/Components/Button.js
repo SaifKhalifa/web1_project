76847a9a9e772bc94c5cbcc2bd6cf1e3960c5e62
rahafhamayel <rahafhamayel12@gmial.com>
2023-12-17 16:45:02 +0200

    component

---
+++ web1-project-team-23/src/Components/Button.js
@@ -0,0 +1,9 @@
+const Button=()=>{
+return (
+   <div className="d-grid gap-2 d-md-block">
+  <button className="btn btn-primary" type="button"><i class="bi bi-file-earmark-play"></i>Watch now</button>
+</div>
+
+)
+}
+export default Button
