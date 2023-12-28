ca6337ba7d5307941f531e97853add59b2ef4fdb
Saif Khalifa <63987779+SaifKhalifa@users.noreply.github.com>
2023-12-28 22:46:28 +0200

    delete old files

--- web1-project-team-23/src/App.js
+++ web1-project-team-23/src/App.js
@@ -1,35 +1,8 @@
 import './App.css';
-import Card from './Components/card/Card';
-import Header from './Components/header/Headr'
-import Footer from './Components/Footer/Footer';
-import NavBar from "./Components/NavBar/NavBar";
-import NewComment from './Components/Main Video Card/NewComment';
-import VideoCard from './Components/Main Video Card/VideoCard';
-import VideoTest from "./Components/Main Video Card/VideoTest";
-import Offical_section from './Components/Yousef/SectionOffical';
-import Music_section from './Components/Yousef/SectionMusic';
+
 function App() {
   return (
-    <>
-      <NavBar/>
-      
-      {/* <VideoCard/> */}
-
-      <VideoTest/>
-      <NewComment/>
-      
-      {/* <Header/>
-      <Card/> */}
-
-      {/*Yousef*/}
-      {/* <Offical_section />
-      <Music_section/> */}
-      {/*Yousef*/}
-      
-      {/*Saif*/}
-      <Footer/>
-    </>
-    
+        
   );
 }
 
